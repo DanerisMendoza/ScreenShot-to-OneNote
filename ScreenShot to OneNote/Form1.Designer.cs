6fa90e0cd0f7e4a8e381bc28e8fccdc497fe5cf8
dane <danerismendoza096@gmail.com>
2022-08-12 11:40:13 +0800

    v1

---
+++ ScreenShot to OneNote/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ScreenShot_to_OneNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 179);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(130, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(12, 197);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(97, 32);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Undo";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 235);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(225, 28);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(249, 232);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ScreenShot to OneNote/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenShot_to_OneNote
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getProcess();
+            if (screenLength == 1){
+                //screen1
+                this.Size = new Size(260, 310);
+            }
+            else{
+                //screen2
+                comboBox1.Visible = false;
+            }
+            
+        }
+        //set specific foreground app as active
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        String toReturn = null;
+         int screenLength = Screen.AllScreens.Length;
+
+        private void button1_Click(object sender, EventArgs e){                    
+            if (screenLength == 1){
+                //screen 1 only block          
+                if (comboBox1.Text == ""){
+                    MessageBox.Show("Select where the active foreground will return");
+                    return;
+                }         
+                SendKeys.SendWait("{PRTSC}");            
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Image = Clipboard.GetImage();
+                pasteToOneNoteSingleScreen();            
+                return;
+            }
+
+            //else multiplse screen block
+            int screenNumber = 1;
+            int targetScreen = 2;
+            foreach (Screen screen in Screen.AllScreens){
+                if (screenNumber == targetScreen){
+                Bitmap screenshot = new Bitmap(screen.Bounds.Width, screen.Bounds.Height, PixelFormat.Format32bppArgb);
+                Graphics memoryGraphics = Graphics.FromImage(screenshot);
+                memoryGraphics.CopyFromScreen(screen.Bounds.X, screen.Bounds.Y, 0, 0, screen.Bounds.Size, CopyPixelOperation.SourceCopy);                       
+                pictureBox1.Image = screenshot;
+                Clipboard.SetImage(screenshot);
+                pasteToOneNoteMultiScreen();     
+                }
+                screenNumber++;
+            }
+        }
+
+        public void pasteToOneNoteMultiScreen(){
+                var prc = Process.GetProcessesByName("onenote");
+                if (prc.Length > 0){
+                    SetForegroundWindow(prc[0].MainWindowHandle);
+                    Thread.Sleep(320);
+                    SendKeys.SendWait("^{v}");
+                    prc = Process.GetProcessesByName("Screenshot to OneNote");
+                    if (prc.Length > 0)                 
+                        SetForegroundWindow(prc[0].MainWindowHandle);                                                  
+                }
+            
+        }
+
+        public void pasteToOneNoteSingleScreen() {
+                var prc = Process.GetProcessesByName("onenote");
+                if (prc.Length > 0){
+                    SetForegroundWindow(prc[0].MainWindowHandle);
+                    Thread.Sleep(700);
+                    SendKeys.SendWait("^{v}");
+                    Thread.Sleep(700);
+                    prc = Process.GetProcessesByName(toReturn);
+                    if (toReturn.Equals("chrome") || toReturn.Equals("msedge")){
+                        int i = 0;
+                        Process[] processes = Process.GetProcessesByName(toReturn);                  
+                        foreach (var process in Process.GetProcessesByName(toReturn)){
+                            SetForegroundWindow(processes[i].MainWindowHandle);
+                            i++;
+                        }          
+                    }
+                    else if (prc.Length > 0)
+                        SetForegroundWindow(prc[0].MainWindowHandle);
+                    prc = Process.GetProcessesByName("Screenshot to OneNote");
+                    if (prc.Length > 0)
+                        SetForegroundWindow(prc[0].MainWindowHandle);
+            }
+             
+        }
+
+        public void getProcess() {
+            List<string> arrList = new List<string>();
+            Process[] processCollection = Process.GetProcesses();
+            foreach (Process p in processCollection){              
+                if(!arrList.Contains(p.ProcessName))
+                arrList.Add(p.ProcessName);
+            }
+           
+            foreach(string s in arrList){
+                comboBox1.Items.Add(s);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e){
+            toReturn = comboBox1.Text;
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e){
+            toReturn = comboBox1.Text;
+        }
+
+    }
+}
